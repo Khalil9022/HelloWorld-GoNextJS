b5390cf8d9d6cbce9514ddd73d97f696fc50b3d1
Khalil Attalla Firdia <khalilattalla2209@gmail.com>
2022-09-28 17:16:12 +0700

    change h1 index

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home() {
 
   return (
     <div className='container mx-auto'>
-      <h1 className='text-sky-400'>Selamat Datang</h1>
+      <h1 className='text-sky-400'>Selamat Datang Di website</h1>
       <br></br>
       {error && <div>Failed to load{error.toString()}</div>}
       {
